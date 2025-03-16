eada31367398175f9f8c7222d0c511c2b36408b4
Muhammad Farrel Asyraf Abrar <farrelabrar@gmail.com>
2025-03-16 21:45:12 +0700

    Fixed model display in admin

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,51 @@
 from django.contrib import admin
 from .models import Parameter, DataAyam, DataAyamHistory, CustomUser, Alat
-# Register your models here.
-admin.site.register(Parameter)
-admin.site.register(DataAyam)
-admin.site.register(DataAyamHistory)
-admin.site.register(Alat)
-admin.site.register(CustomUser)
+# Register your models here
+
+class ParameterAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        verbose_name = 'Parameter'
+        verbose_name_plural = 'Parameter'
+
+class AlatAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        verbose_name = 'Alat'
+        verbose_name_plural = 'Alat'
+
+
+class DataAyamAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        verbose_name = 'Data ayam'
+        verbose_name_plural = 'Data ayam'
+
+class DataAyamAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        verbose_name = 'Data ayam'
+        verbose_name_plural = 'Data ayam'
+
+class DataAyamHistoryAdmin(admin.ModelAdmin):
+
+    class Meta:
+        verbose_name = 'Riwayat data ayam'
+        verbose_name_plural = 'Riwayat data ayam'
+
+class UserAdmin(admin.ModelAdmin):
+
+    class Meta:
+        verbose_name = 'Pengguna'
+        verbose_name_plural = 'Pengguna'
+
+
+
+admin.site.register(Parameter, ParameterAdmin)
+admin.site.register(DataAyam, DataAyamAdmin)
+admin.site.register(DataAyamHistory, DataAyamHistoryAdmin)
+admin.site.register(Alat, AlatAdmin)
+admin.site.register(CustomUser, UserAdmin)
 admin.site.site_header = 'SIGMA ADMIN'
 admin.site.site_title = 'Portal administrasi SIGMA'
 admin.site.index_title = 'Selamat datang ke administrasi SIGMA!'
