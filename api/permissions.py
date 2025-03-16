fb16b4b5133d0dfe5bd22190ba19c02b96520cc6
Muhammad Farrel Asyraf Abrar <farrelabrar@gmail.com>
2025-03-16 21:33:47 +0700

    Fixed urls.py

--- api/permissions.py
+++ api/permissions.py
@@ -6,4 +6,5 @@ class IsAlatRole(BasePermission):
     def has_permission(self, request, view):
 
         return request.user.is_authenticated and getattr(request.user, "role", None) == "alat"
-    
+
+
--- sigma_backend/urls.py
+++ sigma_backend/urls.py
@@ -4,12 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-def redirect_to_site(request):
+def redirect_to_site_login(request):
     return("https://www.sigma-ta.com/")
 
 
 
 urlpatterns = [
+    path('', redirect_to_site_login),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include api app URLs
 ] 
