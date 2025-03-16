32ce706ea3149582e5842ae265dc1bbb68f9ea8d
Muhammad Farrel Asyraf Abrar <farrelabrar@gmail.com>
2025-03-16 21:14:11 +0700

    fixed __str__ on CustomUser

--- api/models.py
+++ api/models.py
@@ -35,7 +35,10 @@ class CustomUser(AbstractUser):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.username} - Role {self.role} - email {self.email}"
+
+        if self.role == 'alat':
+            return f"{self.username} - {self.role}"
+        return f"{self.username} - {self.role} - {self.email}"
 
 #model untuk alat
 class Alat(models.Model):
