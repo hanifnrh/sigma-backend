0a04463168fe2c821aafc102492bc2606e5e7b98
Muhammad Farrel Asyraf Abrar <farrelabrar@gmail.com>
2025-04-17 08:32:34 +0700

    added the url for alat login

--- api/urls.py
+++ api/urls.py
@@ -15,6 +15,7 @@ from .views import (
     RegisterView,
     AlatCreateUpdateView,
     AlatListView,
+    AlatLoginView,
     UserDetailView,
 )
 urlpatterns = [
@@ -32,6 +33,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
     path('login/', UserLoginView.as_view(), name="login"),
     path('user/profile/update/', UserDetailView.as_view(), name = 'update-profile' ),
+    path('alat/login/', AlatLoginView().as_view(), name = 'alat-login' ),
     path('alat/', AlatCreateUpdateView().as_view(), name="alat-create-update"),
     path('alat/list/', AlatListView().as_view(), name="alat-list" ),
 ]
